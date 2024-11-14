61d83a65404c941d93c2fb1dd5ef458ef08afbec
Nguyễn Minh Tuấn <tuannguyengodry07@gmail.com>
2024-11-14 19:33:27 +0700

    change

--- website/game.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-let text=document.getElementById("click");
-let a = 0;
-function hi() {
-    a+=1;
-    text.innerHTML=a;
-}
-let data = fs.ReadFileSync("data.txt","utf-8");
