8fe75d8034342f6fef64963093ed237df08c7293
Nguyễn Minh Tuấn <tuannguyengodry07@gmail.com>
2024-11-22 00:12:40 +0700

    add another avatar

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
 let text=document.getElementById("click");
 let a = 0;
+let img = document.getElementById("bnn");
 function hi() {
     a+=1;
     text.innerHTML=a;
+}
+function abc() {
+    if (img.getAttribute("src") == "/resources/banana.png") {
+        img.src = "/resources/banana2.png"
+    }
+    else {
+        img.src = "/resources/banana.png"
+    }
 }
