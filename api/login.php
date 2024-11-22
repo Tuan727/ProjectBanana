45031838904f4792d367aeaf8c257dcd8b0c06dd
Nguyễn Minh Tuấn <tuannguyengodry07@gmail.com>
2024-11-22 09:04:57 +0700

    login

--- api/login.php
+++ api/login.php
@@ -1,5 +1,10 @@
 <?php
     $user = $_POST["user"];
     $pass = $_POST["pass"];
-    echo $user." ".$pass."";
+    if ($user == "admin" && $pass == "admin") {
+        header("Location: ../home-login.html")
+    }
+    else {
+        echo "wrong password"
+    }
 ?>
